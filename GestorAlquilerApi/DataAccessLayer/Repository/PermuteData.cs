0d2f5100fe430db5965bf47187017a4f85710be7
Muhammad <mh.haidor@gmail.com>
2023-04-20 14:24:07 +0200

    IPermuteData

---
+++ GestorAlquilerApi/DataAccessLayer/Repository/PermuteData.cs
@@ -0,0 +1,22 @@
+using System;
+using GestorAlquilerApi.DataAccessLayer.Data;
+using GestorAlquilerApi.DataAccessLayer.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestorAlquilerApi.DataAccessLayer.Repository
+{
+    public class PermuteData<T> : IPermuteData<T>
+    {
+        private readonly ApiContext _context;
+
+        public PermuteData(ApiContext context)
+        {
+            _context = context;
+        }
+        
+        public void ModifiedState(T branch) =>
+            _context.Entry(branch).State = EntityState.Modified;
+
+        public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+    }
+}
