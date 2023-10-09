2d971a4696189f9977b449ccde4669645f6a48b8
mohasb <mh.haidor@gmail.com>
2023-10-09 14:39:30 +0200

    final commit

--- GestorAlquilerApi/Program.cs
+++ GestorAlquilerApi/Program.cs
@@ -17,7 +17,7 @@ var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
-{
+{ 
 
     options.AddPolicy(
         name: MyAllowSpecificOrigins,
